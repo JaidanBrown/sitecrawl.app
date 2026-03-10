57b7cbe83d9e5ed0263f21acbacd4251282bdd77
Jaidan <jaidanbrown1999@gmail.com>
2026-03-10 15:12:22 +0000

    builds

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_DAV2vrLd.mjs';
-import { manifest } from './manifest_BNkVRFAq.mjs';
+import { manifest } from './manifest_D4-cECeK.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -27,7 +27,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "c5607595-6e77-41df-9984-75f9aa25b84c",
+    "middlewareSecret": "6e6cd246-54f5-4fc4-9c34-5148e52a24ca",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,9 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    maxDuration: 30,
+  }),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
